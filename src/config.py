1e57cf38e6256fb61500472b6aa1eeb0d34fdcbb
Tanguy NICOLAS <tanguy-nicolas@outlook.fr>
2024-03-14 22:45:28 +0100

    🚀 Bootstrap FastAPI! With example.

--- src/config.py
+++ src/config.py
@@ -1,9 +1,11 @@
 # e.g. global env vars
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     db_user: str
     db_pass: str
+
+    model_config = SettingsConfigDict(env_file="../.env")
 
 settings = Settings()
